386decd30488dae27db2ee8c0bfc792b439233b8
eugeneyovbak <eugene.yovbak@spsoft.com>
2019-11-25 18:10:36 +0200

    added usecases covered by tests

---
+++ lib/domain/entities/number_trivia.dart
@@ -0,0 +1,11 @@
+import 'package:meta/meta.dart';
+
+class NumberTrivia {
+  final String text;
+  final int number;
+
+  NumberTrivia({
+    @required this.text,
+    @required this.number,
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,25 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      body: Container()
     );
   }
 }
